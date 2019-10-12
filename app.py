c8d52eee87cd401fa32fb828037eab703eae79f7
Sanchit <sanchitcop19@Gmail.com>
2019-10-12 01:43:22 -0400

    added gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "<h1>Hullo</h1>"
+
+@app.route('/status/<uid>')
+def status(uid):
+    field = [[100, 110, 120, 120, 120],
+             [100, 110, 120, 120, 130],
+             [100, 110, 120, 130, 130],
+             [100, 110, 130, 130, 130]]
+    return jsonify({'field': field})
 
 if __name__ == '__main__':
     app.run()
