cd0606e919b31a81fd32decd233a0d5061d998ff
Sanchit <sanchitcop19@Gmail.com>
2019-10-12 21:57:11 -0400

    whatevs

--- app.py
+++ app.py
@@ -17,9 +17,27 @@ def home():
 def predict(uid):
     return jsonify({'prediction': get_prediction(datetime.now())})
 
+def squish_health(health):
+    minimum, maximum = 0, 100
+    health = ((maximum - minimum) * (health - minimum)) // (1000)
+    return health
+
+@app.route('/update-realtime/<uid>/<health>')
+def update_realtime(uid, health):
+    health = squish_health(int(health))
+    with open("store.json") as file:
+        store = json.load(file)
+    if not store:
+        store = {}
+    store['users'][str(uid)] = health
+    with open("store.json", "w") as file:
+        json.dump(store, file)
+    return make_response(jsonify({}))
+
 
 @app.route('/update/<uid>/<health>')
 def update(uid, health):
+    health = squish_health(int(health))
     store = None
     with open("store.json") as file:
         store = json.load(file)
@@ -42,16 +60,32 @@ def status(uid):
         store = json.load(file)
     health = store['users'][str(uid)]
     import random
-    health = [
+    health_matrix = [
         [health]*5,
         [health-(random.randint(5, 15))]*5,
         [health-(random.randint(5, 15))]*5,
         [health-(random.randint(5, 15))]*5,
         [health-(random.randint(5, 15))]*5
     ]
-    response = make_response(jsonify({'health': health}))
+    # 5x5
+    # [1,1,1,1,1]
+    # [1,1,1,1,1]
+    # [1,1,1,0,1]
+    # [1,1,1,1,1]
+    # [1,1,1,1,1]
+    sensor_x = 3
+    sensor_y = 2
+    for row, row_vals in enumerate(health_matrix):
+        for col, location in enumerate(row_vals):
+            if row == sensor_y and col == sensor_x:
+                health_matrix[row][col] = health
+            else:
+                euclidean_distance = (row - sensor_y)**2 + (col - sensor_x)**2
+                health_matrix[row][col] = health - 3*euclidean_distance
+
+    response = make_response(jsonify({'health': health_matrix}))
     return response
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port = 5006)
